66b407f4c50867d03421eb8ce4b8f8274d6ac188
FISEI-LAB1-PC11 <andrescarrera212@gmail.com>
2023-12-12 18:17:46 -0500

    Primer Commit

---
+++ app/src/main/java/esteban/g/cannongame2/Line.java
@@ -0,0 +1,15 @@
+package esteban.g.cannongame2;
+import android.graphics.Point;
+
+public class Line
+{
+    public Point start; // starting Point
+    public Point end; // ending Point
+
+    // default constructor initializes Points to (0, 0)
+    public Line()
+    {
+        start = new Point(0, 0); // start Point
+        end = new Point(0, 0); // end Point
+    } // end method Line
+}
